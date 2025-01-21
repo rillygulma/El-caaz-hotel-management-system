c44b7e9aa08510073ce8c28282d81ac03e1df898
Rilwanu Idris <Rillygulma@gmail.com>
2025-01-21 20:22:10 +0100

    Fix GET Request in room-review

--- src/app/api/room-reviews/[id]/route.ts
+++ src/app/api/room-reviews/[id]/route.ts
@@ -1,8 +1,9 @@
 import { getRoomReviews } from '@/libs/apis';
 import { NextResponse } from 'next/server';
+import { NextRequest } from 'next/server';
 
 export async function GET(
-  req: Request,
+  request: NextRequest,
   { params }: { params: { id: string } }
 ) {
   const roomId = params.id;
@@ -12,10 +13,10 @@ export async function GET(
 
     return NextResponse.json(roomReviews, {
       status: 200,
-      statusText: 'Succesful',
+      statusText: 'Successful',
     });
   } catch (error) {
-    console.log('Getting Review Failed', error);
+    console.error('Getting Review Failed', error);
     return new NextResponse('Unable to fetch', { status: 400 });
   }
 }
